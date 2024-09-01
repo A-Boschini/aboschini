2bb982baa98681e259371b730ed00d402b02ed6b
Alberto Boschini <albertoboschinicr@gmail.com>
2024-09-01 12:12:14 -0600

    SELF

--- person_blueprint.py
+++ person_blueprint.py
@@ -5,18 +5,21 @@ class Person:
         self.age = age
         self.personID = personID
 
+    def display_data(self):
+        print("Hi, my name is ", self.name, ",my age is", self.age, ", and my personal ID is", self.personID)
+
 person01 = Person("Alberto", 58, 123)
 person02 = Person("Gino", 61, 13)
 person03 = Person("Fiona", 18, 5)
 
-print(person01.name)
-print(person01.age)
-print(person01.personID)
+#print(person01.name)
+#print(person01.age)
+#print(person01.personID)
 
-print(person02.name)
-print(person02.age)
-print(person02.personID)
+#print(person02.name)
+#print(person02.age)
+#print(person02.personID)
 
-print(person03.name)
-print(person03.age)
-print(person03.personID)
+#print(person03.name)
+#print(person03.age)
+#print(person03.personID)
