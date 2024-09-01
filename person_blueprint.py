844f8d90ca08d98e31aa90fed47d5d355d48d1b6
Alberto Boschini <albertoboschinicr@gmail.com>
2024-09-01 11:02:58 -0600

    Corrected File 09-1-2024

--- person_blueprint.py
+++ person_blueprint.py
@@ -11,11 +11,11 @@ person03 = Person("Fiona", 18, 5)
 
 print(person01.name)
 print(person01.age)
-print(person01.personIDage)
+print(person01.personID)
 
 print(person02.name)
 print(person02.age)
-print(person02.personIDage)
+print(person02.personID)
 
 print(person03.name)
 print(person03.age)
