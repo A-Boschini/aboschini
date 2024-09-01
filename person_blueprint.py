bf2d47d10273cadcfd4c9b3bec2f22de8c5fc5f0
Alberto Boschini <albertoboschinicr@gmail.com>
2024-09-01 10:56:12 -0600

    changes

---
+++ person_blueprint.py
@@ -0,0 +1,22 @@
+class Person:
+    """Person main class"""
+    def __init__(self, name, age, personID):
+        self.name = name
+        self.age = age
+        self.personID = personID
+
+person01 = Person("Alberto", 58, 123)
+person02 = Person("Gino", 61, 13)
+person03 = Person("Fiona", 18, 5)
+
+print(person01.name)
+print(person01.age)
+print(person01.personIDage)
+
+print(person02.name)
+print(person02.age)
+print(person02.personIDage)
+
+print(person03.name)
+print(person03.age)
+print(person03.personID)
