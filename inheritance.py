a18bd83f4cf1bc6b84c67375e612d0d815f482ae
Alberto Boschini <albertoboschinicr@gmail.com>
2024-09-03 12:24:32 -0600

    Inhereritance02-sept03-2024

---
+++ inheritance.py
@@ -0,0 +1,18 @@
+# Parent Class
+def Vehicle_data(self):
+    print("Hello from the Vehicle Class!")
+
+
+
+
+# Chiled Class
+class Car(Vehicle):
+    def Car_data(self):
+        print("Hello from the Car Class!")
+
+# Object based on Car
+car01 = Car()
+
+# Get Vehicle data
+car01.Vehicle_data()
+car01.Car_data()
