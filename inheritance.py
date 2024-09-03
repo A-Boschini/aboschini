f512c5251ee8740e48a3072a3106a383b4d3590c
Alberto Boschini <albertoboschinicr@gmail.com>
2024-09-03 12:30:57 -0600

    Inheritance-04-sept03-2024

--- inheritance.py
+++ inheritance.py
@@ -8,9 +8,15 @@ class Car(Vehicle):
     def Car_data(self):
         print("Hello from the Car Class!")
 
+class Motor(Vehicle):
+    def Motor_data(self):
+        print("Hello from the Motor Class!")
+
 # Object based on Car
 car01 = Car()
+car02 = Motor()
 
 # Get Vehicle data
 car01.Vehicle_data()
 car01.Car_data()
+car02.Motor_data()
